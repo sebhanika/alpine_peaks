9dedcb44d1faa4bfe39181d8602320ccb2ce1837
sebhanika <hanikasebastian@gmail.com>
2022-08-12 16:50:22 +0200

    Create peaks project

---
+++ prep_mountain_names.R
@@ -0,0 +1,108 @@
+
+## ---------------------------
+##
+## Script name: Mountains
+##
+## Topic: Webmap German Mountain Names in the Alps
+##
+## Author: Sebastian Hanika
+##
+## Date Created: 12/08/2022
+## ---------------------------
+##
+## Notes:
+##   
+##
+## ---------------------------
+
+
+# Libraries and setup -----------------------------------------------------
+
+
+#install.packages("osmdata")
+
+library(tidyverse)
+library(sf)
+library(osmdata)
+library(downloader)
+library(leaflet)
+library(shiny)
+
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+
+
+
+# Load mountain areas -----------------------------------------------------
+
+url_alps <- "https://www.eea.europa.eu/data-and-maps/data/european-mountain-areas/european-mountain-areas/european-mountain-areas/at_download/file"
+
+download(url = url_alps, dest="mountain_areas.zip", mode="wb") # downloads zip folder into current directory
+unzip("mountain_areas.zip", exdir = "data/mountain_areas") # unzips file into current working directory
+
+
+alps.load <- read_sf(dsn ="data/mountain_areas/m_massifs_v1.shp") %>% 
+  filter(mm_code == 1)  #filter only for alps
+
+alps.reproj <- st_transform(alps.load, 4326)
+alps <- st_make_valid(alps.reproj)
+
+rm(alps.load, alps.reproj)
+gc()
+
+# Load OSM data -----------------------------------------------------------
+
+# set extent of box
+alp.ext <-  unname(st_bbox(alps, crs = st_crs(4326)))
+
+# build query
+osm.query <- opq(bbox = alp.ext,
+                 nodes_only = TRUE,
+                 timeout = 500) %>% 
+  add_osm_feature(key = 'natural', value = 'peak') 
+
+
+# call query, takes a long time
+peaks.query <- osmdata_sf(osm.query)
+
+
+peaks.raw <- peaks.query$osm_points %>% 
+  select(c(osm_id, name, geometry))
+
+saveRDS(peaks.raw, file = "temp.peaks")
+
+# filter only points in alps 
+peaks.alps <- st_intersection(peaks.raw, alps)
+
+rm(peaks.query)
+gc()
+
+# Clean name data ---------------------------------------------------------
+
+
+# most typical endings
+endings <- paste0(c("berg", "spitze", "spitz", "kogel", "horn", "joch", 
+                    "stein", "eck", "ling", "stock", "kopf"), " ")
+
+endings_filt <- paste(endings, collapse = "|")
+
+
+
+peaks.ends <- peaks.alps %>% 
+  mutate(name = str_replace_all(name, "\\-", " - "),
+         name = str_replace_all(name, "\\/", " / "),
+         name = str_replace_all(name, "  "," "),
+         name = paste0(name, " ")) %>% #adding whitespace at the end
+  mutate(type_end = str_extract(name, endings_filt)) %>%  #extract values if
+  mutate(type_end = str_trim(if_else(type_end == "spitze ",
+                                     "spitz ",
+                                     type_end), "right"),
+         type_end_lab = paste0("-", type_end),
+         type_end_lab = ifelse(type_end_lab == "-spitz", "-spitz(e)", type_end_lab)) %>% 
+  filter(type_end != is.na(type_end))
+
+
+
+# saveRDS(peaks.ends, "temp_peaks_ends")
+
+#loadRDS
+peaks <- readRDS("temp_peaks_ends")
